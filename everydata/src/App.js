ec032a970143e8df8d9366d6bcb35ff14ac4d143
wlals92 <tngod0921@gmail.com>
2023-05-07 03:51:35 +0900

    jimin 로그인, 회원가입 js 구현 1차 완료

--- everydata/src/App.js
+++ everydata/src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
+import React from "react";
+import {Routes, Route, Link} from 'react-router-dom'
+import Main from "./pages/Main";
 import './App.css';
+import './css/main.css';
+import './css/nav.css';
+import './css/footer.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav>
+        <ul id="nav-menu">
+          <li><Link to="/manageCredit">학점 관리</Link></li>
+          <li><Link to="/">대학정보</Link></li>
+          <li>
+            <Link>강의</Link>
+            <ul id="nav-menu_sub">
+              <li><Link to="/searchTimeTable">강의목록</Link></li>
+              <li><Link to="/recommendProcess">강의 추천</Link></li>
+            </ul>
+          </li>
+        </ul>
+        <div id="nav-logo">
+          <img id="nav-logo_img" src="img/logo-green.png" alt="메인로고" height="30"/>
+          <Link to="/">
+            <span>EVERY</span><span className="nav-logo_data">DATA</span>
+          </Link>
+        </div>
+        <button id="nav-logout_button" type="button" onclick="">로그아웃</button>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Main/>}/>
+        <Route path="/manageCredit" element={<manageCredit/>}/>
+        <Route path="/searchTimeTable" element={<searchTimeTable/>}/>
+        <Route path="/recommendProcess" element={<recommendProcess/>}/>
+      </Routes>
+      <footer>
+        <div id="footer-left">
+          <div>2023 - 1 소프트웨어설계PBL : EVERYDATA</div>
+          <div>팀 오합지졸 - 남서린 | 민희진 | 이지민 | 훌랑</div>
+          <div>Copyright 2023. 오합지졸 All rights reserved.</div>
+        </div>
+        <div id="footer-right">
+          <img src="img/logo-white.png" width="80"/>
+        </div>
+      </footer>
     </div>
   );
 }
--- js/signup.js
+++ js/signup.js
@@ -1,88 +1,223 @@
 //이지민 작성
-const backend_base_url = ""
-const frontend_base_url = ""
 
-async function handleSignup(){
-    const signup_data = {
-        id : document.getElementById("id"),
-        password : document.getElementById("password").value,
-        repassword : document.getElementById("repassword").value,
-        username : document.getElementById("username").value,
-        academic_number : document.getElementById("academic_number").value,
-        academic_status : document.getElementById("status").value,
-        curriculum_year : document.getElementById("curriculum_year").value,
-        grade : document.getElementById("grade").value,
-        major : document.getElementById("major").value,
-        double_major : document.getElementById("double_major").value,
-        minor : document.getElementById("minor").value
+//유효성 체크 메세지 보여주는 함수
+//에러일 때 red vs #FF8540(주황) 고민중
+const showMessage = (targetId, message, isError) => {
+    const borderColor = isError ? '#FF8540' : '';
+    const textColor = isError ? '#FF8540' : '#A5A5A5';
+    $(`#${targetId}`).css('border-color', borderColor);
+    $(`#${targetId}-checktext`).css('color', textColor).text(message);
+};
+
+//아이디 유효성 검사
+const id_inputcheck = () => {
+    //영문자, 숫자로 이루어진 6~20자의 문자열
+    const idReg = /^[a-zA-Z0-9]{6,20}$/;
+    const id = document.getElementById("id").value;
+
+    if (idReg.test(id)){
+        //유효한 아이디 형식
+        showMessage('id', '', false);
+        return true;
     }
+    else if (id === ""){
+        //비어있을 경우
+        showMessage('id', '아이디를 입력해주세요.', true);
+        return false;
+    }
+    else {
+        //유효하지 않은 아이디 형식
+        showMessage('id', '6~20자의 영문자, 숫자로 입력해주세요.', true);
+        return false;
+    }
+};
+
+//비밀번호 유효성 검사
+const password_inputcheck = () => {
+    //영문자, 숫자, 특수문자(!@#$%^&*) 중 2종류 이상을 조합한 8~20자의 문자열
+    const passwordReg = /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[!@#$%^&*])[a-zA-Z\d!@#$%^&*]{8,20}$/;
+    const password = document.getElementById("password").value;
+
+    if (passwordReg.test(password)){
+        //유효한 비밀번호 형식
+        showMessage('password', '', false);
+        return true;
+    }
+    else if (password === ""){
+        //비어있을 경우
+        showMessage('password', '비밀번호를 입력해주세요.', true);
+        return false;
+    }
+    else {
+        //유효하지 않은 비밀번호 형식
+        showMessage('password', '8~20자로 영문자, 숫자, 특수문자 중 2종류 이상을 포함하여 입력해주세요.', true);
+        return false;
+    }
+};
+
+//비밀번호 확인 유효성 검사
+const repassword_inputcheck = () => {
+    const password = document.getElementById("password").value;
+    const repassword = document.getElementById("repassword").value;
     
-    const response = await fetch(`${backend_base_url}/users/`,{
-        headers:{
-            'Content-type': 'application/json'
-        },
-        method:'POST',
-        body:JSON.stringify(signup_data)
+    if (repassword === "") {
+        //비어있을 경우
+        showMessage('repassword', '비밀번호를 입력해주세요.', true);
+        return false;
     }
-    )
-
-    response_json = await response.json()
-
-    if (response.status == 200){
-        window.location.replace(`${frontend_base_url}/index.html`);
-    }else{
-        alert(response.status)
+    else if (password === repassword){
+        //비밀번호와 비밀번호 확인이 일치하는 경우
+        showMessage('repassword', '비밀번호가 일치합니다.', false);
+        $("#repassword-checktext").css('color', '#40BBCA');
+        return true;
     }
-}
-
-async function handleLogin(){
-    console.log("handle login")
-
-    const login_data = {
-        id : document.getElementById("id"),
-        password : document.getElementById("password").value
+    else {
+        //비밀번호와 비밀번호 확인이 일치하지 않는 경우
+        showMessage('repassword', '일치하지 않습니다. 비밀번호를 다시 입력해주세요.', true);
+        return false;
     }
+};
+
+//이름 유효성 검사
+const name_inputcheck = () => {
+    //한글로 된 문자열
+    const nameReg = /^[가-힣]+$/;
+    const name = document.getElementById("name").value;
+  
+    if (nameReg.test(name)) {
+        //유효한 이름 형식
+        showMessage('name', '', false);
+        return true;
+    } else if (name === "") {
+        //비어있을 경우
+        showMessage('name', '이름을 입력해주세요.', true);
+        return false;
+    } else {
+        //유효하지 않은 이름 형식
+        showMessage('name', '한글로 입력해주세요.', true);
+        return false;
+    }
+};
+  
+//학번 유효성 검사
+const academic_number_inputcheck = () => {
+    //숫자로 된 문자열
+    const academic_numberReg = /^[0-9]+$/;
+    const academic_number = document.getElementById("academic_number").value;
     
-    const response = await fetch(`${backend_base_url}/users/api/token/`,{
-        headers:{
-            'Content-type': 'application/json'
-        },
-        method:'POST',
-        body:JSON.stringify(login_data)
+    if (academic_numberReg.test(academic_number)) {
+        //유효한 학번 형식
+        showMessage('academic_number', '', false);
+        return true;
+    } else if (academic_number === "") {
+        //비어있을 경우
+        showMessage('academic_number', '학번을 입력해주세요.', true);
+        return false;
+    } else {
+        //유효하지 않은 학번 형식
+        showMessage('academic_number', '숫자로 입력해주세요.', true);
+        return false;
     }
-    )
+};
 
-    response_json = await response.json()
-    console.log(response_json.access)
-
-    if (response.status == 200){
-        localStorage.setItem("access", response_json.access)
-        localStorage.setItem("refresh", response_json.refresh)
-
-        const base64Url = response_json.access.split('.')[1];
-        const base64 = base64Url.replace(/-/g, '+').replace(/_/g,'/');
-        const jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
-            return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
-        }).join(''));
-
-        localStorage.setItem("payload", jsonPayload);
-        window.location.replace(`${frontend_base_url}/`);
-    }else{
-        alert(response.status)
+//상태, 교과적용년도, 학년, 학과 입력 체크
+const select_inputcheck = (inputId, checktextId) => {
+    const select = document.getElementById(inputId);
+    const checktext = document.getElementById(checktextId);
+  
+    if (select.value === "선택") {
+        //선택하지 않은 경우
+        select.style.borderColor = "red";
+        checktext.style.color = "red";
+        checktext.innerHTML = "선택해주세요.";
+        return false;
+    } else {
+        //선택한 경우
+        select.style.borderColor = "";
+        checktext.innerHTML = "";
+        return true;
     }
+};
+
+//파일 입력 체크
+const file_inputcheck = () => {
+    const grade = document.getElementById("grade").value;
+    const fileInput = document.getElementById('file');
+    const fileName = fileInput.value.trim();
+    //pdf 형식 제한
+    const fileEx = /(\.pdf)$/i;
+
+    if (fileName === ''){
+        //1학년일 경우 입력하지 않아도 됨
+        if (grade === '1') {
+            showMessage('file', '1-1 신입생의 경우 이수내역확인표는 추가하지 않아도 됩니다.', false);
+            return true;
+        }
+        else {
+            showMessage('file', '2, 3, 4학년은 이수내역확인표를 추가해주세요.', true);
+            return false;
+        }
+    }
+    //pdf 형식으로만 입력 가능
+    else if (!fileEx.exec(fileName)) {
+        showMessage('file', 'pdf형식으로 입력해주세요.', true);
+        return false;
+    }
+    else {
+        showMessage('file', '1-1 신입생의 경우 이수내역확인표는 추가하지 않아도 됩니다.', false);
+        return true;
+    }
+
 }
 
-async function handleLogout(){
-    localStorage.removeItem("access")
-    localStorage.removeItem("refresh")
-    localStorage.removeItem("payload")
-    alert("로그아웃되었습니다.")
-    location.reload()
-}
+//전체 유효성 검사
+const validateForm = () => {
+    if (!id_inputcheck()){
+        //아이디 유효성 검사 실패
+        return false;
+    }
+    if (!password_inputcheck()){
+        //비밀번호 유효성 검사 실패
+        return false;
+    }
+    if (!repassword_inputcheck()){
+        //비밀번호 확인 유효성 검사 실패
+        return false;
+    }
+    if (!name_inputcheck()){
+        //이름 유효성 검사 실패
+        return false;
+    }
+    if (!academic_number_inputcheck()){
+        //학번 유효성 검사 실패
+        return false;
+    }
+    if (!select_inputcheck()){
+        //선택 항목 미입력
+        return false;
+    }
+    if (!file_inputcheck()){
+        //파일 유효성 검사 실패
+        return false;
+    }
+    //모든 입력이 유효한 경우
+    return true;
+};
 
-const id_inputcheck =()=>{
-    const id_check = /^[a-z]+[a-z0-9]{5,19}$/g;
-}
-const password_inputcheck =()=>{
-    const password_check = /^.*(?=^.{5,19}$)(?=.*\d)(?=.*[a-zA-Z])(?=.*[~,!,@,#,$,*,(,),=,+,_,.,|]).*$/;
-}
+const form = document.querySelector('form');
+
+form.addEventListener("submit", function(event) {
+    event.preventDefault(); //폼 제출 이벤트 취소
+
+    const isValid = validateForm();
+
+    //유효성 검사 결과에 따라 form submit
+    if (isValid) {
+        console.log("제출 완료");
+        form.submit();
+    }
+    else {
+        alert("입력한 내용을 다시 확인해주세요.");
+        return false;
+    }
+});
